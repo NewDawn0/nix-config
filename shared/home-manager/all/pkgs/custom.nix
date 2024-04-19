45f4e9ed8720b348f4e190f3af8d28b5ce826f4c
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-04-19 03:59:13 +0200

    rename ds to dirStack

--- shared/home-manager/all/pkgs/custom.nix
+++ shared/home-manager/all/pkgs/custom.nix
@@ -4,6 +4,6 @@
   };
 
   config = lib.mkIf config.pkgs-customCfg.enable {
-    home.packages = with pkgs; [ ansi ds ex gen nixie-clock note translate up ];
+    home.packages = with pkgs; [ ansi dirStack ex gen nixie-clock note translate up ];
   };
 }
