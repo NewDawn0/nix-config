2ca022d9d95c3e0eeb88c9bbd2c67ab92ff625d4
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-04-19 05:55:18 +0200

    format

--- shared/home-manager/all/pass.nix
+++ shared/home-manager/all/pass.nix
@@ -1,9 +1,10 @@
 { config, lib, ... }: {
-  options = { password-storeCfg.enable = lib.mkEnableOption "enable password-store config"; };
+  options = {
+    password-storeCfg.enable =
+      lib.mkEnableOption "enable password-store config";
+  };
 
   config = lib.mkIf config.password-storeCfg.enable {
-    programs.password-store = {
-      enable = true;
-    };
+    programs.password-store = { enable = true; };
   };
 }
--- shared/home-manager/all/pkgs/custom.nix
+++ shared/home-manager/all/pkgs/custom.nix
@@ -4,6 +4,15 @@
   };
 
   config = lib.mkIf config.pkgs-customCfg.enable {
-    home.packages = with pkgs; [ ansi dirStack ex gen nixie-clock note translate up ];
+    home.packages = with pkgs; [
+      ansi
+      dirStack
+      ex
+      gen
+      nixie-clock
+      note
+      translate
+      up
+    ];
   };
 }
