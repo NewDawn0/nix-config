596f38a8b00974dac03914a97084d8929d45a950
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-05-09 20:42:59 +0200

    add nvim config

---
+++ shared/home-manager/all/nvim/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }: {
+  options = { nvimCfg.enable = lib.mkEnableOption "enable neovim config"; };
+  config = lib.mkIf config.alacrittyCfg.enable {
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      withNodeJs = true;
+      withPython3 = true;
+      withRuby = true;
+    };
+  };
+}
