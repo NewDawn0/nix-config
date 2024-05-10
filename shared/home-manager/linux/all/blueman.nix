0e33f3e5c38b831f3c01463fcbb8bece078b7bec
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-05-10 02:02:57 +0200

    rm obsolete

--- shared/home-manager/linux/all/blueman.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, ... }: {
-  options = {
-    bluemanCfg.enable = lib.mkEnableOption "enable blueman service config";
-  };
-
-  config = lib.mkIf config.bluemanCfg.enable {
-    services.blueman-applet = { enable = true; };
-  };
-}
--- shared/home-manager/linux/all/default.nix
+++
@@ -1,7 +0,0 @@
-{ lib, ... }: {
-  imports = [ ./firefox.nix ./cava.nix ./blueman.nix ];
-
-  firefoxCfg.enable = lib.mkDefault true;
-  bluemanCfg.enable = lib.mkDefault true;
-  cavaCfg.enable = lib.mkDefault true;
-}
