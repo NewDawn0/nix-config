138ae9bef44af8a2d97b19757b92e89d7cc6a811
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-05-19 01:53:20 +0200

    add colorscheme to fzf

--- shared/home-manager/all/fzf.nix
+++ shared/home-manager/all/fzf.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: {
+{ config, lib, scheme, ... }: {
   options = { fzfCfg.enable = lib.mkEnableOption "enable fzf config"; };
 
   config = lib.mkIf config.fzfCfg.enable {
@@ -7,6 +7,16 @@
       enableBashIntegration = false;
       enableZshIntegration = false;
       tmux.enableShellIntegration = true;
+      colors = with scheme.withHashtag; {
+        prompt = base0E;
+        hl = base0C;
+        "hl+" = base15;
+        fg = base06;
+        "fg+" = base0A;
+        pointer = base08;
+        bg = base11;
+        "bg+" = base11;
+      };
     };
   };
 }
