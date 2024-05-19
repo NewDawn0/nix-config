f1dded24db3d9f6edacdbbafa441fb9f424c9252
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-05-19 18:30:07 +0200

    dynamic imports and modules

--- shared/darwin/default.nix
+++ shared/darwin/default.nix
@@ -1,6 +1,15 @@
-{ lib, ... }: {
-  imports = [ ./system.nix ./homebrew.nix ];
-
-  homebrewCfg.enable = lib.mkDefault true;
-  systemCfg.enable = lib.mkDefault true;
-}
+{ lib, ... }: 
+with builtins;
+with lib;
+let
+  # Useful vars
+  files = filter (f: f != "default.nix") (attrNames (readDir ./.));
+  mergeAttrs = attrs: foldl' (acc: next: acc // next) { } attrs;
+  # @OUT <imports>
+  imports = map (f: ./${f}) files;
+  # @OUT <enable>
+  enableCfgs = map (f: attrsets.setAttrByPath [ f "enable" ] (mkDefault true))
+    (map (f: "darwin-${f}Cfg") (map (removeSuffix ".nix") files));
+  # @FINAL <imports, enable>
+  out = { inherit imports; } // mergeAttrs enableCfgs;
+in out
