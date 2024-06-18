7dcc622e91c63bf7604510aedbf901a93e2040ce
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-06-19 00:10:24 +0200

    Use ndnvim + nix-custom-pkgs provided overlay

--- shared/flake/overlays.nix
+++ shared/flake/overlays.nix
@@ -1,24 +1,5 @@
 { inputs }: [
   inputs.rust-overlay.overlays.default
-  (final: prev: {
-    ansi = inputs.nix-custom-pkgs.packages.${prev.system}.ansi;
-    dirStack = inputs.nix-custom-pkgs.packages.${prev.system}.dirStack;
-    ex = inputs.nix-custom-pkgs.packages.${prev.system}.ex;
-    gen = inputs.nix-custom-pkgs.packages.${prev.system}.gen;
-    nixie-clock = inputs.nix-custom-pkgs.packages.${prev.system}.nixie-clock;
-    note = inputs.nix-custom-pkgs.packages.${prev.system}.note;
-    notify = inputs.nix-custom-pkgs.packages.${prev.system}.notify;
-    rgpt = inputs.nix-custom-pkgs.packages.${prev.system}.rgpt;
-    translate = inputs.nix-custom-pkgs.packages.${prev.system}.translate;
-    up = inputs.nix-custom-pkgs.packages.${prev.system}.up;
-  })
-  (final: prev:
-    if prev.stdenv.isDarwin then {
-      homebrew-manager =
-        inputs.nix-custom-pkgs.packages.${prev.system}.homebrew-manager;
-      mac-apps-archive =
-        inputs.nix-custom-pkgs.packages.${prev.system}.mac-apps-archive;
-    } else
-      { })
-  (final: prev: if prev.stdenv.isLinux then { } else { })
+  inputs.ndnvim.overlays.default
+  inputs.nix-custom-pkgs.overlays.default
 ]
