77fd02e44d3ba76bd81d45ec88805559444de841
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-11 22:21:21 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "NewDawn0's system flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-systems.url = "github:nix-systems/default";
+    ndnvim.url = "github:NewDawn0/ND-Nvim";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-darwin = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:LnL7/nix-darwin";
+    };
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.home-manager.follows = "home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-custom-pkgs = {
+      url = "github:NewDawn0/nix-custom-pkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nix-systems.follows = "nix-systems";
+    };
+  };
+
+  outputs =
+    inputs@{ self, nixpkgs, nixpkgs-unstable, nix-darwin, home-manager, ... }:
+    let util = import ./lib { inherit inputs nixpkgs nixpkgs-unstable; };
+    in {
+      nixosConfigurations = { };
+      darwinConfigurations = {
+        NewDawn0-mbpro2020 = nix-darwin.lib.darwinSystem {
+          modules = [ ./hosts/NewDawn0-mbpro2020 ];
+          specialArgs = {
+            inherit self home-manager inputs;
+            inherit (util.mkArgs "x86_64-darwin" "NewDawn0-mbpro2020" "tom")
+              userInfo pkgs unstable overlays;
+          };
+        };
+      };
+    };
+}
