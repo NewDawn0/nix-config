d3a2f822c8e80c53a21aac2276a9a95fab1accc3
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-11 23:29:38 +0200

    fmt

--- hosts/NewDawn0-mbpro2020/default.nix
+++ hosts/NewDawn0-mbpro2020/default.nix
@@ -1,9 +1,9 @@
-{ config, home-manager, inputs, lib, overlays, pkgs
-, self, unstable, userInfo, util, ... }:
+{ config, home-manager, inputs, lib, overlays, pkgs, self, unstable, userInfo
+, util, ... }:
 let
   importPath = import ../../lib/importPath.nix {
-    inherit config home-manager inputs lib overlays
-      pkgs self unstable userInfo util;
+    inherit config home-manager inputs lib overlays pkgs self unstable userInfo
+      util;
   };
 in {
   imports = importPath ../../shared;
