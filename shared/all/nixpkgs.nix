3f86ae9a0926129dac597c219afc87e6d66c8ce7
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-11 23:45:07 +0200

    add gc & substitutor configs

--- shared/all/nixpkgs.nix
+++ shared/all/nixpkgs.nix
@@ -7,5 +7,25 @@
   };
   # Nix settings
   services.nix-daemon.enable = true;
-  nix.settings.experimental-features = "nix-command flakes";
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      substituters =
+        [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+      user = "root";
+      interval = {
+        Hour = 2;
+        Minute = 0;
+        Weekday = 0;
+      };
+    };
+  };
 }
