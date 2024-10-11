c299d43ce3d4b4e948fef9c7f27f3914737e9a36
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-11 22:20:54 +0200

    host config

---
+++ hosts/NewDawn0-mbpro2020/default.nix
@@ -0,0 +1,12 @@
+{ config, home-manager, inputs, lib, overlays, pkgs
+, self, unstable, userInfo, util, ... }:
+let
+  importPath = import ../../lib/importPath.nix {
+    inherit config home-manager inputs lib overlays
+      pkgs self unstable userInfo util;
+  };
+in {
+  imports = importPath ../../shared;
+  programs.zsh.enable = true;
+  environment.systemPackages = with pkgs; [ ndnvim ];
+}
