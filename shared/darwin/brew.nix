96adaad2e5a5b8484c24d3fc7c5937d7b4653fa1
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-12 02:55:31 +0200

    add apps

--- shared/darwin/brew.nix
+++ shared/darwin/brew.nix
@@ -1,10 +1,44 @@
 { inputs, lib, userInfo, ... }: {
-  inputs.nix-homebrew.darwinModules.nix-homebrew {
-    nix-homebrew = {
-      enable = true;
-      enableRosetta = if lib.hasPrefix "aarch64" userInfo.system then true else false;
-      user = userInfo.userName;
-      mutableTaps = false;
+  nix-homebrew = {
+    enable = true;
+    enableRosetta = if lib.hasPrefix "aarch64" userInfo.system then true else false;
+    user = userInfo.userName;
+    mutableTaps = true;
+  };
+  homebrew = {
+    enable = true;
+    brews = [
+      "mas"
+      "mpv"
+      "openssl@3"
+    ]; 
+    casks = [
+      "amethyst"
+      "appcleaner"
+      "balenaetcher"
+      "firefox"
+      "firefox"
+      "github"
+      "hammerspoon"
+      "iina"
+      "languagetool"
+      "microsoft-excel"
+      "microsoft-outlook"
+      "microsoft-powerpoint"
+      "microsoft-word"
+      "obs"
+      "raycast"
+      "whatsapp"
+    ];
+    masApps = {
+      Shazam = 897118787;
+      XCode = 497799835;
     };
-  }
+    global.autoUpdate = false;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+      upgrade = true;
+    };
+  };
 }
