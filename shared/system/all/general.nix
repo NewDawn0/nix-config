d879ad4241c9dfdf662afbb8d162838a4bed460d
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-14 00:20:52 +0200

    rm activationScripts.gc

--- shared/system/all/general.nix
+++ shared/system/all/general.nix
@@ -1,13 +1,6 @@
 { self, ... }: {
   system = {
-    activationScripts.gc = {
-      enable = true;
-      text = ''
-        nix-store --gc
-      '';
-    };
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 5;
   };
-
 }
