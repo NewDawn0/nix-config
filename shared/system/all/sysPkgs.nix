6fc8b921b505cc0dd05f80b29d59876e59d1b007
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-10-14 13:54:26 +0200

    Add dev shell manager

--- shared/system/all/sysPkgs.nix
+++ shared/system/all/sysPkgs.nix
@@ -1,4 +1,30 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let 
+nix-dev = pkgs.writeShellScriptBin "nix-dev" ''
+  if [ -d $NIX_INSTALL_DIR/shells ]; then
+    if [ $# -gt 1 ]; then
+      case $1 in
+        "run") 
+          nix develop ''${NIX_INSTALL_DIR}#$2 -c zsh
+        ;;
+        *)
+          echo "Unknown subcommand $1"
+          exit 1
+        ;;
+      esac
+    else
+      echo "Run available shells using 'nix-dev run <shell>'"
+      echo "Shells:"
+      for file in $NIX_INSTALL_DIR/shells/*; do
+        name=$(basename ''${file%.nix})
+        echo " '-> $name"
+      done
+    fi
+  else
+    echo '$NIX_INSTALL_DIR/shells directory does not exist'
+    echo 'Set NIX_INSTALL_DIR to where your system config is located'
+    exit 1
+  fi
+''; in {
   environment.systemPackages = with pkgs; [
     (ndnvim.override { langs.all.enabled = true; })
     coreutils
@@ -10,6 +36,7 @@
     gnused
     killall
     less
+    nix-dev
     nixfmt-classic
     pkg-config
     ripgrep
