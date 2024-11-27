f8d44f8a70093798597a9da4b10860059bf2bea9
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-11-27 11:58:57 +0100

    fix fastfetch

--- shared/home-manager/all/fastfetch.nix
+++ shared/home-manager/all/fastfetch.nix
@@ -1,15 +1,56 @@
-{ ... }: {
+{ pkgs, userInfo, ... }:
+let
+  mkConfigSource = logo:
+    (pkgs.formats.json { }).generate "_" {
+      "$schema" =
+        "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      inherit logo;
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "kernel"
+        "uptime"
+        "packages"
+        "icons"
+        "terminal"
+        "cpu"
+        "gpu"
+        "memory"
+        "swap"
+        "disk"
+        "localip"
+        "break"
+        "colors"
+      ];
+    };
+in {
   programs.fastfetch = { enable = true; };
   xdg.configFile."fastfetch/ibm.jsonc" = {
-    source = ../../nonNix/fastfetch/ibm.jsonc;
-  };
-  xdg.configFile."fastfetch/logos/ibm.txt" = {
-    source = ../../nonNix/fastfetch/logos/ibm.txt;
+    source = mkConfigSource {
+      source = "${userInfo.userHome}/.config/fastfetch/logos/ibm.txt";
+      color = {
+        "1" = "blue";
+        "2" = "yellow";
+        "3" = "green";
+        "4" = "red";
+      };
+    };
   };
   xdg.configFile."fastfetch/megamind.jsonc" = {
-    source = ../../nonNix/fastfetch/megamind.jsonc;
+    source = mkConfigSource {
+      source = "${userInfo.userHome}/.config/fastfetch/logos/megamind.txt";
+      color = {
+        "1" = "blue";
+        "2" = "yellow";
+        "3" = "green";
+        "4" = "red";
+      };
+    };
   };
+  xdg.configFile."fastfetch/logos/ibm.txt" = { source = ./fastfetch/ibm.txt; };
   xdg.configFile."fastfetch/logos/megamind.txt" = {
-    source = ../../nonNix/fastfetch/logos/megamind.txt;
+    source = ./fastfetch/megamind.txt;
   };
 }
